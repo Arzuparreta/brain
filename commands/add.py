51a388564cc4833e6d228b916870be7740040ef6
Arzuparreta <125204298+Arzuparreta@users.noreply.github.com>
2026-03-17 15:18:56 +0100

    add note extension selector

--- commands/add.py
+++ commands/add.py
@@ -1,6 +1,6 @@
 import click
 import subprocess
-from config import TEXT_EDITOR
+from config import TEXT_EDITOR, NOTE_EXTENSION
 from logic import get_brain_data_dir
 
 FILES_DIR = get_brain_data_dir()
@@ -10,7 +10,7 @@ FILES_DIR = get_brain_data_dir()
 @click.argument("name")
 def add_files(name):
     """Stick something to your brain"""
-    path = FILES_DIR / f"{name}.md"
+    path = FILES_DIR / f"{name}.{NOTE_EXTENSION}"
     try:
         subprocess.run([TEXT_EDITOR, str(path)])
     except subprocess.CalledProcessError as e:
--- config.py
+++ config.py
@@ -3,5 +3,8 @@ from pathlib import Path
 # directory for brain data (You can change this directly!!)
 FILES_DIR = Path(__file__).parent.resolve() / "brain_data"
 
+# default note extension
+NOTE_EXTENSION = "md"
+
 # default text editor for editing notes
 TEXT_EDITOR = "nvim"  # Change this to your preferred text editor
