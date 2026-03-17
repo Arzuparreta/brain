d60d7e905688feea51d9d9ca3389f9280fc0737e
Arzuparreta <125204298+Arzuparreta@users.noreply.github.com>
2026-03-17 18:10:08 +0100

    adjust edit to new settings logic

--- commands/edit.py
+++ commands/edit.py
@@ -1,26 +1,26 @@
 import click
 import subprocess
-from config import TEXT_EDITOR
-from logic import get_brain_data_dir
+from settings import settings
 
-FILES_DIR = get_brain_data_dir()
+editor = settings["TEXT_EDITOR"]
+dir = settings["FILES_DIR"]
 
 
 @click.command(name="edit")
 @click.argument("name", required=False)
 def edit_files(name):
     """Edit the brain data files."""
-    file_path = FILES_DIR
+    file_path = dir
     if name:
-        file_path = FILES_DIR / f"{name}.md"
+        file_path = dir / f"{name}.md"
     if file_path.exists():
         try:
-            subprocess.run(TEXT_EDITOR.split() + [str(file_path)], check=True)
+            subprocess.run(editor.split() + [str(file_path)], check=True)
         except subprocess.CalledProcessError as e:
             click.echo(f"Error opening editor: {e}")
     else:
-        file_path = FILES_DIR
+        file_path = dir
         try:
-            subprocess.run(TEXT_EDITOR.split() + [str(file_path)])
+            subprocess.run(editor.split() + [str(file_path)])
         except subprocess.CalledProcessError as e:
             click.echo(f"Error opening editor: {e}")
