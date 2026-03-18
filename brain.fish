5d66105a9b04788d2cf4fb0f641c7478be35bb21
Arzuparreta <125204298+Arzuparreta@users.noreply.github.com>
2026-03-18 10:31:53 +0100

    fix completions bad definitions and remove junk

--- brain.fish
+++ brain.fish
@@ -1,10 +1,10 @@
-function __brain_complete_edit
-    brain _complete_edit
+function __brain_complete_files
+    brain _complete_files
 end
 
 complete -c brain -f
-complete -c brain -n __fish_use_subcommand -a "edit list new"
+complete -c brain -n __fish_use_subcommand -a "edit list add remove"
 
 complete -c brain \
     -n "__fish_seen_subcommand_from edit" \
-    -a "(__brain_complete_edit)"
+    -a "(__brain_complete_files)"
--- logic.py
+++ logic.py
@@ -1,5 +1,4 @@
 from settings import settings
-import click
 from config import FILES_DIR
 
 
@@ -13,8 +12,3 @@ def get_brain_data_dir():
     """Return the brain data and create it if it doesn't exist."""
     FILES_DIR.mkdir(exist_ok=True)
     return FILES_DIR
-
-
-@click.group()
-def cli():
-    pass
