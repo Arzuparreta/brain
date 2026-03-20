51c23969c81ebf1b6b2b5206227b8929d930e157
Arzuparreta <125204298+Arzuparreta@users.noreply.github.com>
2026-03-20 23:39:00 +0100

    add completions to see command

--- brain.fish
+++ brain.fish
@@ -3,9 +3,9 @@ function __brain_complete_files
 end
 
 complete -c brain -f
-complete -c brain -n __fish_use_subcommand -a "edit list add remove rename"
+complete -c brain -n __fish_use_subcommand -a "edit list add remove rename see"
 
 complete -c brain \
-    -n "__fish_seen_subcommand_from edit remove" \
+    -n "__fish_seen_subcommand_from edit remove see" \
     -a "(__brain_complete_files)"
 complete -c brain -n "__fish_seen_subcommand_from rename" -a "(__brain_complete_files)"
--- commands/install_completions.py
+++ commands/install_completions.py
@@ -11,8 +11,8 @@ function __brain_complete_files
 end
 
 complete -c brain -f
-complete -c brain -n "__fish_use_subcommand" -a "edit add list remove rename"
-complete -c brain -n "__fish_seen_subcommand_from edit remove" -a "(__brain_complete_files)"
+complete -c brain -n "__fish_use_subcommand" -a "edit add list remove rename see"
+complete -c brain -n "__fish_seen_subcommand_from edit remove see" -a "(__brain_complete_files)"
 complete -c brain -n "__fish_seen_subcommand_from rename" -a "(__brain_complete_files)"
 """
 
