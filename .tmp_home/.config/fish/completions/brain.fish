cc346b8988241aba4af7c138ed706b21c3bce63a
Arzuparreta <rubenpenarubio81@gmail.com>
2026-03-21 09:10:21 +0100

    fixes

--- .tmp_home/.config/fish/completions/brain.fish
+++
@@ -1,9 +0,0 @@
-
-function __brain_complete_files
-    brain complete_files
-end
-
-complete -c brain -f
-complete -c brain -n "__fish_use_subcommand" -a "edit add list remove rename"
-complete -c brain -n "__fish_seen_subcommand_from edit remove" -a "(__brain_complete_files)"
-complete -c brain -n "__fish_seen_subcommand_from rename" -a "(__brain_complete_files)"
